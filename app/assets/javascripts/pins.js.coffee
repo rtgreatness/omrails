05e80dcd5c2621c0534eca6cd779f129c928581b
Robert Tsui <rtsui001@gmail.com>
2013-06-08 13:00:58 -0700

    Connect Amazon S3

--- app/assets/javascripts/pins.js.coffee
+++ app/assets/javascripts/pins.js.coffee
@@ -2,6 +2,6 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-JQuery ->
-	$('#pins').imagesLoaded ->
-		$('#pins').masonry itemSelector: ".box"
+jQuery ->
+    $('#pins').imagesLoaded ->
+        $('#pins').masonry itemSelector: ".box"
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,10 +1,10 @@
 class PinsController < ApplicationController
-  before_filter :authenticate_user!, except: (:index)
+  before_filter :authenticate_user!, except: [:index]
   # GET /pins
   # GET /pins.json
   def index
     @pins = Pin.order("created_at desc")
-
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @pins }
